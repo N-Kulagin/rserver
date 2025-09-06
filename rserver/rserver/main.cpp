a17a917c13e7e0be3bf3f35ef5cafb8512372fe8
N. Kulagin <n.kulagin.it@gmail.com>
2025-09-07 01:12:03 +0300

    Added doxyfile

--- rserver/rserver/main.cpp
+++ rserver/rserver/main.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file main.cpp
+ * @author N. Kulagin (n.kulagin.it@gmail.com)
+ * 
+ * @date 2025-09-07
+ * @copyright Copyright (c) 2025
+ */
+
 #include "network/socket.h"
 
 int main() 
