17abdc511d6e01d460136eb5dd95843fc6d97bb7
N. Kulagin <n.kulagin.it@gmail.com>
2025-09-07 16:37:39 +0300

    Added FTXUI submodule

--- rserver/rserver-admin/main.cpp
+++ rserver/rserver-admin/main.cpp
@@ -1,6 +1,23 @@
 #include "network/socket.h"
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <iostream>
 
 int main() 
 {
     rserver::sayHello();
+
+    // TODO: add autoformatting
+
+    using namespace ftxui;
+ 
+    Element document = hbox({
+        text("left")   | border,
+        text("middle") | border | flex,
+        text("right")  | border,
+    });
+ 
+    auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+    Render(screen, document);
+    screen.Print();
 }
