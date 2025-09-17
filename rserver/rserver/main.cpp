0adc8696bdad4f0d6ee4ecd9c49c141dbae05105
N. Kulagin <n.kulagin.it@gmail.com>
2025-09-17 14:25:11 +0300

    Renamed cmake project

--- rserver/rserver/main.cpp
+++ rserver/rserver/main.cpp
@@ -10,6 +10,11 @@
 
 #include "network/socket.h"
 
+static void foo(int x)
+{
+    std::cout << x << '\n';
+}
+
 int main(int argc, char** argv)
 {
     CLI::App app{"App description"};
@@ -18,12 +23,22 @@ int main(int argc, char** argv)
     std::string filename = "default";
     app.add_option("-f,--file", filename, "A help string");
 
-    if (true) {
+    int x;
+    int y = 100;
+    double z = 2.5;
+    double ww = 2.5;
+    double www = 3.5;
+    double wwww = 4.5;
+    double zzz = 3.5;
+    foo(zzz);
+    foo(z);
+    int zz = 3.5;
+    int zzzz = 3.4;
+
+    if (true)
         return 1;
-    }
-    else {
+    else
         return 2;
-    }
 
     CLI11_PARSE(app, argc, argv);
 
